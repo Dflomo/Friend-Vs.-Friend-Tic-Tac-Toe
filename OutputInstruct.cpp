d0e55428050037a4b49a581acf6d3c9be86a8924
Dflomo <dflomo@gmail.com>
2015-10-08 10:55:26 -0700

    Create OutputInstruct.cpp

---
+++ OutputInstruct.cpp
@@ -0,0 +1,40 @@
+#include "headers.h"
+
+/****************************************************************
+ VOID FUNCTION - OutputInstruct
+ _______________________________________________________________
+ Outputs a set of Themed Instructions to play TicTacToe
+ _______________________________________________________________
+ PRE-CONDITIONS
+		 There are no post conditions for this functions
+
+ POST-CONDITIONS
+		 There are no post conditions for this functions
+***************************************************************/
+
+void OutputInstruct()
+{
+	cout << "-------------------------------------------------"
+			"---------\n";
+	cout << "*A Scroll Labeled \"INSTRUCTION SCROLL\" lies opened in "
+			"front you you*\n";
+	cout << "-------------------------------------------------"
+			"----------\n";
+	cout << "Welcome and Good Morrow to you Champion!\n";
+	cout << "It appears you have stumbled upon the TIC TAC TOE"
+			" battle lands!\n";
+	cout << "As dreadful as it sounds, fear not!\n";
+	cout << "You shall be equipped with both your intellect AND!\nan army"
+			" of X's or O's to do battle in your stead.\n";
+	cout << "But first, know this... You are playing on the deadly\n3x3 "
+			"field of Fallen Champions!\n";
+	cout << "In order for your army to move forth, you must select a two"
+			" number combination\nbetween 1 and 3.\n";
+	cout << "These numbers signify strategic coordinates, for which you "
+			"army may move to!\n";
+	cout << "But be weary, few Champions have made it back alive...\nwill "
+			"you be the select few?\n";
+	cout << "PREPARE YOURSELF!\n";
+	cout << "-------------------------------------------------------\n\n";
+
+}
