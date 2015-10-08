867ecf694aa11e8bfa32c555d18db988299f2e5a
Dflomo <dflomo@gmail.com>
2015-10-08 11:04:30 -0700

    Create PrintHeaders.cpp

---
+++ PrintHeaders.cpp
@@ -0,0 +1,23 @@
+#include "headers.h"
+
+/****************************************************************
+ FUNCTION - PrintHeader
+ _______________________________________________________________
+ Passes by value, the Assignment Name, Type and Number
+ Outputs accordingly
+ _______________________________________________________________
+ PRE-CONDITIONS
+
+
+ POST-CONDITIONS
+		 There are no post conditions for this functions
+***************************************************************/
+void PrintHeaders()
+{
+	cout << left;
+	cout << "**************************************************************\n";
+	cout << "Programmed by : Derek Florimonte";
+
+	cout << "\n**************************************************************\n";
+	cout << right;
+}
